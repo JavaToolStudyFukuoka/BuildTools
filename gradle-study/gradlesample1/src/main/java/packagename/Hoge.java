9d1939a17adb73a0f2ec67cb5b1b4160812c21ea
ligun <choonggeun@gmail.com>
2015-11-20 18:47:26 +0900

    Gradleサンプルプロジェクト追加

---
+++ gradle-study/gradlesample1/src/main/java/packagename/Hoge.java
@@ -0,0 +1,14 @@
+package packagename;
+
+public class Hoge {
+    private int a,b;
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static void main(String... args) {
+        System.out.println("Hello world!");
+    }
+}
+
